c2e56a339e87560127420139b5ea22e16dddd57c
Talita Layane <talita.layanes@gmail.com>
2026-06-25 16:03:33 +0100

    chore: update ruby version, improvemente seeds and some updates for projeject

--- Gemfile
+++ Gemfile
@@ -1,12 +1,13 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.7.4'
+ruby '3.1.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.7'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 1.1'
+gem 'will_paginate', '~> 3.3'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,11 @@ puts "admin created"
 
 User.create( name: "User", email: "user@user.com", password: "123456")
 puts "user created"
+
+# Create books
+Book.create(name: "The Great Gatsby", author: "F. Scott Fitzgerald", category: "Fiction")
+Book.create(name: "To Kill a Mockingbird", author: "Harper Lee", category: "Fiction")
+Book.create(name: "1984", author: "George Orwell", category: "Science Fiction")
+Book.create(name: "Pride and Prejudice", author: "Jane Austen", category: "Romance")
+Book.create(name: "The Catcher in the Rye", author: "J.D. Salinger", category: "Fiction")
+puts "5 books created"

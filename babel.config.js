a31bd58e6eb94b9e4b1a87f3bb1cb604ed1f9827
Talita Layane <talita.layanes@gmail.com>
2026-07-07 14:11:07 +0100

    feature: add configuraniton for React envoriment

--- babel.config.js
+++ babel.config.js
@@ -34,7 +34,8 @@ module.exports = function(api) {
           modules: false,
           exclude: ['transform-typeof-symbol']
         }
-      ]
+      ],
+      ['@babel/preset-react', { runtime: 'automatic' }]
     ].filter(Boolean),
     plugins: [
       'babel-plugin-macros',
